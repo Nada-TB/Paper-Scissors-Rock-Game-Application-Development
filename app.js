afd3c838524b0c5c7a0640a54f192d42c6d73094
Nada <nada.tebba@hotmail.fr>
2020-12-21 18:20:03 +0100

    upload new image

--- app.js
+++ app.js
@@ -1,55 +1,54 @@
-'use strict';
+"use strict";
 /************************************variables*********************************************** */
-const cards=document.querySelectorAll('#player img');
-let computerChoice=document.getElementById('computerChoice');
-let computerScrore=0;
-let playerScore=0;
-
+const cards = document.querySelectorAll("#player img");
+let computerChoice = document.getElementById("computerChoice");
+let computerScrore = 0;
+let playerScore = 0;
 
 /*************************************************functions************************************ */
-function scaleCard(element, size){
-    element.style.transform='scale('+size+')';
+function scaleCard(element, size) {
+  element.style.transform = "scale(" + size + ")";
 }
 
-function fetchMessage(element, message){
-    element.textContent=message;
+function fetchMessage(element, message) {
+  element.textContent = message;
 }
-function changeSrc(element, newSrc){
-    element.src=newSrc;
+
+function changeSrc(element, newSrc) {
+  element.src = newSrc;
 }
-function getScore(compare){
-    if(compare==='the computer wins, hard luck '){
-        computerScrore++;
-    }else if(compare==='you win, kudos '){
-        playerScore++
-    }else{
-        playerScore++;
-        computerScrore++;
-    }
+
+function getScore(compare) {
+  if (compare === "the computer wins, hard luck ") {
+    computerScrore++;
+  } else if (compare === "you win, kudos ") {
+    playerScore++;
+  } else {
+    playerScore++;
+    computerScrore++;
+  }
 }
 
 /***********************************************Events****************************************** */
 
-for(let i=1; i<cards.length; i++){
-    cards[i].addEventListener('click', function(){
-        let card= new Card(cards[i].src);
-        let result=card.getWinner();
-        let newSrc= result.split(' ').pop(); 
-        getScore(result.replace(newSrc,''));
-        scaleCard(cards[i],1.2);
-        changeSrc(computerChoice,'images/'+newSrc+'.jpg');
-        document.getElementById('result').style.display='block';
-        document.getElementById('score').style.display='block';
-        fetchMessage(document.getElementById('result'), result.replace(newSrc,''));
-        fetchMessage(document.getElementById('computerScore'),  +computerScrore);
-        fetchMessage(document.getElementById('playerScore'), playerScore);
-        
-        setTimeout(() => {
-            scaleCard(this, 1);
-            fetchMessage(document.getElementById('result'), ' play again!');
-            changeSrc(computerChoice, '');
-            
-        }, 2000);
-             
-    })
+for (let i = 1; i < cards.length; i++) {
+  cards[i].addEventListener("click", function () {
+    let card = new Card(cards[i].src);
+    let result = card.getWinner();
+    let newSrc = result.split(" ").pop();
+    getScore(result.replace(newSrc, ""));
+    scaleCard(cards[i], 1.2);
+    changeSrc(computerChoice, "images/" + newSrc + ".jpg");
+    document.getElementById("result").style.display = "block";
+    document.getElementById("score").style.display = "block";
+    fetchMessage(document.getElementById("result"), result.replace(newSrc, ""));
+    fetchMessage(document.getElementById("computerScore"), computerScrore);
+    fetchMessage(document.getElementById("playerScore"), playerScore);
+
+    setTimeout(() => {
+      scaleCard(this, 1);
+      fetchMessage(document.getElementById("result"), " play again!");
+      changeSrc(computerChoice, "images/question.png");
+    }, 2000);
+  });
 }
--- class_card.js
+++ class_card.js
@@ -1,64 +1,61 @@
-'use strict';
+"use strict";
 
-class Card{
-    constructor(src){
-        this.src=src;
+class Card {
+  constructor(src) {
+    this.src = src;
+  }
+
+  getCardName() {
+    this.src = this.src.split("/");
+    this.src = this.src.pop();
+    this.src = this.src.replace(".jpg", "");
+    return this.src;
+  }
+
+  getComputerChoice() {
+    let random = Math.random() * 100 + 1;
+    if (random < 33) {
+      return "paper";
+    } else if (random < 66) {
+      return "scissors";
+    } else {
+      return "rock";
     }
+  }
 
-    getCardName(){
-       this.src= this.src.split('/');
-       this.src=this.src.pop();
-        this.src=this.src.replace('.jpg', '');
-       return this.src;
-        
-    }
+  getWinner() {
+    let computerChoice = this.getComputerChoice();
+    let playerChoice = this.getCardName();
 
-    getComputerChoice(){
-        let random= (Math.random() * 100) + 1;
-        if(random<33){
-            return 'paper';
-        }else if(random<66){
-            return 'scissors';
-        }else{
-            return 'rock';
+    switch (playerChoice) {
+      case "paper":
+        if (computerChoice === "paper") {
+          return "you both win " + computerChoice;
+        } else if (computerChoice === "scissors") {
+          return "the computer wins, hard luck " + computerChoice;
+        } else {
+          return "you win, kudos " + computerChoice;
         }
+        break;
+      case "scissors":
+        if (computerChoice === "paper") {
+          return "you win " + computerChoice;
+        } else if (computerChoice === "scissors") {
+          return "you both win " + computerChoice;
+        } else {
+          return "the computer wins, hard luck " + computerChoice;
+        }
+        break;
+
+      case "rock":
+        if (computerChoice === "paper") {
+          return "the computer wins, hard luck " + computerChoice;
+        } else if (computerChoice === "scissors") {
+          return "you win, kudos " + computerChoice;
+        } else {
+          return "you both win! " + computerChoice;
+        }
+        break;
     }
-
-    getWinner(){
-       let computerChoice= this.getComputerChoice();
-       let playerChoice=this.getCardName();
-
-       switch(playerChoice){
-           case 'paper':
-               if(computerChoice==='paper'){
-                   return 'you both win '+computerChoice;
-               }else if(computerChoice==='scissors'){
-                   return 'the computer wins, hard luck '+computerChoice;
-               }else{
-                   return 'you win, kudos '+computerChoice;
-               }
-            break;
-            case 'scissors':
-                if(computerChoice==='paper'){
-                    return 'you win '+computerChoice;
-                }else if(computerChoice==='scissors'){
-                    return 'you both win '+computerChoice;
-                }else{
-                    return 'the computer wins, hard luck '+computerChoice;
-                }
-             break;
-
-             case 'rock':
-                if(computerChoice==='paper'){
-                    return 'the computer wins, hard luck '+computerChoice;
-                }else if(computerChoice==='scissors'){
-                    return 'you win, kudos '+computerChoice;
-                }else{
-                    return 'you both win! '+computerChoice;
-                }
-             break;
-
-            
-       }
-    }
+  }
 }
